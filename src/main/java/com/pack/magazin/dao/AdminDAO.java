eb2848643deab52d2f6c10d229d05108d0d26f14
Alexandru Leonte <alexandruleonte06@yahoo.com>
2018-09-21 15:29:25 +0300

    second commit - before namedQuery error

---
+++ src/main/java/com/pack/magazin/dao/AdminDAO.java
@@ -0,0 +1,28 @@
+package com.pack.magazin.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.pack.magazin.entity.Admin;
+import com.pack.magazin.entity.JPAEntityFactoryBean;
+
+@Component
+public class AdminDAO {
+	@Autowired
+	JPAEntityFactoryBean entityFactoryBean;
+	
+	public List<Admin> getAdmins(){
+		EntityManagerFactory emf = entityFactoryBean.getEntityManagerFactory();
+		EntityManager em = emf.createEntityManager();
+		TypedQuery<Admin> adminQuery = em.createNamedQuery("Admin.findAll",Admin.class);
+		List<Admin> admini = adminQuery.getResultList();
+		em.close();
+		return admini;
+	}
+}
--- src/main/java/com/pack/magazin/entity/JPAEntityFactoryBean.java
+++ src/main/java/com/pack/magazin/entity/JPAEntityFactoryBean.java
@@ -1,5 +1,18 @@
 package com.pack.magazin.entity;
 
-public class JPAEntityFactoryBean {
+import javax.annotation.*;
+import javax.persistence.*;
+import org.springframework.stereotype.*;
 
+@Component
+public class JPAEntityFactoryBean {
+	EntityManagerFactory entityManagerFactory;
+	
+	@PostConstruct
+	public void init() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("MagazinSpring");
+	}
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
 }
